7abbdbe9f3f8c7a83ba01f93a6e46a81b427af94
dotha <dotha@DESKTOP-JFEQ0VE>
2023-03-12 23:18:18 +0700

    Add project files.

---
+++ BigSchool2/Controllers/CoursesController.cs
@@ -0,0 +1,27 @@
+﻿using BigSchool2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BigSchool2.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        // GET: Courses
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ BigSchool2/Models/CourseViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BigSchool2.Models
+{
+    public class CourseViewModel
+    {
+        public string Place { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+        }
+    }
+}
